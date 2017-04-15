4b52df8da147f9575bc8a218010c8a9a20f77d39
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-04-15 21:28:37 +0200

    makes build system actually build everything and improves strncpy and memchr

--- libc/include/string.h
+++ libc/include/string.h
@@ -6,6 +6,5 @@ void *memmove(void* dstptr, const void* srcptr, size_t size);
 void *memcpy(void *restrict dstptr, const void *restrict srcptr, size_t size);
 int memcmp(const void *aptr, const void *bptr, size_t size);
 void *memchr(const void *str, int c, size_t n);
-
 char *strcpy(char *dest, const char *src);
 char *strncpy(char *dest, const char *src, size_t n);
--- libc/string/strncpy.c
+++ libc/string/strncpy.c
@@ -3,10 +3,16 @@
 
 char *strncpy(char *dest, const char *src, size_t n){
     //size_t i = 0;
-    for(size_t i = 0; i < n && src[i] != '\0'; i++){
+    size_t i = 0;
+    for( ; i < n && src[i] != '\0'; i++){
         dest[i] = src[i];
         if(src[i+1] == '\0' && i+1 < n){
-            src[i+1] = '\0';
+            dest[i+1] = '\0';
+        }
+    }
+    if(i < n){
+        while(i++ < n){
+            dest[i] = '\0';
         }
     }
     return dest;
